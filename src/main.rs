4b6902460863bce3b4213e97e2703bcd9c3c3296
Tobias Bich <87118266+bitob@users.noreply.github.com>
2022-11-29 17:18:37 +0100

    How to use mut stuff with ReffCells?

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use core::cell::RefCell;
+use core::fmt::Debug;
+
+trait MyTrait: Debug {
+    fn do_something(&mut self);
+}
+
+#[derive(Debug)]
+struct MyStruct {
+    counter: usize,
+}
+
+impl MyStruct {
+    fn new() -> Self {
+        Self { counter: 0 }
+    }
+}
+
+impl MyTrait for MyStruct {
+    fn do_something(&mut self) {
+        self.counter += 1;
+        println!("{self:?}");
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+enum MyEnum<'a> {
+    VarOne(RefCell<&'a dyn MyTrait>),
+    VarTwo(RefCell<&'a dyn MyTrait>),
+}
+
+fn main() {
+    let data = MyStruct::new();
+    let foo = MyEnum::VarOne(RefCell::new(&data));
+    let mut foo_vec: heapless::LinearMap<u16, MyEnum, 8> = heapless::LinearMap::new();
+    let key = 42;
+    foo_vec.insert(key, foo).unwrap();
+
+    match foo_vec.get_mut(&key).unwrap() {
+        MyEnum::VarOne(data) => {
+            let mut data_mut = data.borrow_mut();
+            data_mut.do_something();
+        }
+        MyEnum::VarTwo(data) => {
+            println!("{:?}", data);
+        }
+    }
+}
